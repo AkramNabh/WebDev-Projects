99c7d7e799f37f6b775bed7db02b4aecbd673394
Akram Abuhajar <99180753+AkramNabh@users.noreply.github.com>
2023-09-01 19:58:24 +0300

    A to do list Project

---
+++ NodeJS/To Do List Project/index.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import { render } from 'ejs';
+const app = express();
+const port = 3000;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/",(req, res)=>{
+res.render("index.ejs");
+});
+
+app.post("/submit",(req, res)=>{
+    //req.body.name is the way to retrieve data from it.
+    res.render("index.ejs",{
+        task: req.body["TheTask"],
+        title: req.body["TheTitle"]
+    });
+})
+
+
+app.listen(port, ()=>{
+    console.log(`connected to ${port} successfully`);
+});
